18b528648a9f33e5a3a59afeb1552b8efecd1abd
許均銘 <tel886912338986@gmail.com>
2018-07-15 19:40:37 +0800

    Initial Commit

---
+++ MAYDONTKNOW/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MAYDONTKNOW
+//
+//  Created by CHUNMING HSU on 2018/7/15.
+//  Copyright © 2018年 CHUNMING HSU. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MAYDONTKNOW/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MAYDONTKNOW
+//
+//  Created by CHUNMING HSU on 2018/7/15.
+//  Copyright © 2018年 CHUNMING HSU. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
